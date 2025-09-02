38dd979666f2c508ffa6d44ac9600c1d4f0cd2f6
LuongSon28112004 <sonluong165@gmail.com>
2025-09-02 11:12:49 +0700

    Align Setting UI

--- Assets/Scripts/UI/Button/UILevelGame/ButtonSetting.cs
+++ Assets/Scripts/UI/Button/UILevelGame/ButtonSetting.cs
@@ -7,9 +7,41 @@ public class ButtonSetting : BaseButton
     [SerializeField]
     private GameObject MenuGame;
 
+    [SerializeField]
+    private GameObject ButtonMainMenu;
+
+    [SerializeField]
+    private GameObject ButtonBackToMap;
+
+    [SerializeField]
+    private GameObject Logo;
+
     public override void OnClick()
     {
         var rectTransform = MenuGame.GetComponent<RectTransform>();
-        rectTransform.DOMoveX(4.4f, 0.5f); // di chuyển sang x = 4.2 trong 0.5 giây
+        rectTransform.DOMoveX(4.4f, 0.5f);
+
+        // Logo rơi xuống từ Y = 70 về Y = 19
+        RectTransform logoRect = Logo.GetComponent<RectTransform>();
+        logoRect.DOAnchorPosY(19f, 0.6f)
+            .From(new Vector2(0, 70))
+            .SetEase(Ease.OutBounce);
+
+        // Rung 2 button (shake)
+        ButtonMainMenu.transform.DOShakePosition(
+            0.5f,   // thời gian rung
+            strength: new Vector3(10f, 10f, 0), // biên độ rung theo X,Y
+            vibrato: 15,  // số lần rung
+            randomness: 90,
+            snapping: false,
+            fadeOut: true);
+
+        ButtonBackToMap.transform.DOShakePosition(
+            0.5f,
+            strength: new Vector3(10f, 10f, 0),
+            vibrato: 15,
+            randomness: 90,
+            snapping: false,
+            fadeOut: true);
     }
 }
