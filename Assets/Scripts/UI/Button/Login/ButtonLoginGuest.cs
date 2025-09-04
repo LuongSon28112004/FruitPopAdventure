1514503a4191b778ed6b2ed2b794fa62eba7c6a6
Truong Quang Vinh <vt3839796@gmail.com>
2025-09-04 11:21:46 +0700

    Add UI and refactor code login more flexible

--- Assets/Scripts/UI/Button/Login/ButtonLoginGuest.cs
+++ Assets/Scripts/UI/Button/Login/ButtonLoginGuest.cs
@@ -17,20 +17,15 @@ public class ButtonLoginGuest : BaseButton
             await AuthenticationService.Instance.SignInAnonymouslyAsync();
             Debug.Log("Sign in anonymously succeeded!");
 
-            // Shows how to get the playerID
             Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
 
         }
         catch (AuthenticationException ex)
         {
-            // Compare error code to AuthenticationErrorCodes
-            // Notify the player with the proper error message
             Debug.LogException(ex);
         }
         catch (RequestFailedException ex)
         {
-            // Compare error code to CommonErrorCodes
-            // Notify the player with the proper error message
             Debug.LogException(ex);
         }
     }
--- Assets/Scripts/UI/Button/Login/ButtonLoginUnityPlayer.cs
+++ Assets/Scripts/UI/Button/Login/ButtonLoginUnityPlayer.cs
@@ -10,7 +10,6 @@ public class ButtonLoginUnityPlayer : BaseButton
     {
         await UnityServices.InitializeAsync();
 
-        // Register the Unity Player Accounts sign-in event handler after services initialization.
         PlayerAccountService.Instance.SignedIn += SignInWithUnityAuth;
     }
 
@@ -19,31 +18,24 @@ public class ButtonLoginUnityPlayer : BaseButton
         await StartPlayerAccountsSignInAsync();
     }
 
-    // Call this from a button or other application-specific trigger to begin the sign-in flow.
     private async Task StartPlayerAccountsSignInAsync()
     {
         if (PlayerAccountService.Instance.IsSignedIn)
         {
-            // If the player is already signed into Unity Player Accounts, proceed directly to the Unity Authentication sign-in.
             SignInWithUnityAuth();
             return;
         }
 
         try
         {
-            // This will open the system browser and prompt the user to sign in to Unity Player Accounts
             await PlayerAccountService.Instance.StartSignInAsync();
         }
         catch (PlayerAccountsException ex)
         {
-            // Compare error code to PlayerAccountsErrorCodes
-            // Notify the player with the proper error message
             Debug.LogException(ex);
         }
         catch (RequestFailedException ex)
         {
-            // Compare error code to CommonErrorCodes
-            // Notify the player with the proper error message
             Debug.LogException(ex);
         }
     }
@@ -57,14 +49,10 @@ public class ButtonLoginUnityPlayer : BaseButton
         }
         catch (AuthenticationException ex)
         {
-            // Compare error code to AuthenticationErrorCodes
-            // Notify the player with the proper error message
             Debug.LogException(ex);
         }
         catch (RequestFailedException ex)
         {
-            // Compare error code to CommonErrorCodes
-            // Notify the player with the proper error message
             Debug.LogException(ex);
         }
     }
